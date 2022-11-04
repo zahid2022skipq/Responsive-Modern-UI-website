8d63f218d9cf35a1d173c45aaaed9118364e7aad
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 11:20:34 +0500

    added logo image and its container

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./navbar.css";
+
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links"></div>
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links-logo">
+          <img src={logo} />
+        </div>
+      </div>
     </div>
   );
 };
