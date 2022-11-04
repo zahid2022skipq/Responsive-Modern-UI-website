fb05fdd933b4b91f47ddb23881a94d43d18715bf
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 11:27:01 +0500

    Adding logo image

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./navbar.css";
 
+const logo = "../../assets/assets/logo.png";
+
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links-logo">
-          <img src={logo} />
+          <img src={logo} alt="logo" />
         </div>
       </div>
     </div>
