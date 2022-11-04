1bc16553e00c0f82b9e808c0f15058212fe7f405
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 11:59:09 +0500

    added sign in and sign up button

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -28,6 +28,10 @@ const Navbar = () => {
           </p>
         </div>
       </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign Up</button>
+      </div>
     </div>
   );
 };
