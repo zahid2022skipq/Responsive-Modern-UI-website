9faa0771784d00eac66205ce8cdf36bc84df8a56
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 11:16:33 +0500

    Added Links styles for navbar

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./navbar.css";
 const Navbar = () => {
-  return <div className="gpt3__navbar"></div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links"></div>
+    </div>
+  );
 };
 
 export default Navbar;
