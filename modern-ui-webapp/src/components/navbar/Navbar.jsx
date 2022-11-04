1abc19b8decc6f9c5296a872ea0490dc93d9636e
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 12:20:50 +0500

    added mobile navbar using state

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
-import { RiMenu3Line, RiCloseLine } from "react-icons";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/assets/logo.svg";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -31,6 +33,22 @@ const Navbar = () => {
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign Up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && <div className="gpt3__navbar-menu_container"></div>}
       </div>
     </div>
   );
