6ab6635af7f5df656ae502bdd75247d64c005dc5
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 13:48:48 +0500

    Navbar done

--- modern-ui-webapp/src/containers/header/Header.jsx
+++ modern-ui-webapp/src/containers/header/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./header.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3__header section_padding" id="home">
+      <div className="gpt3__header-content"></div>
+    </div>
+  );
 };
 
 export default Header;
