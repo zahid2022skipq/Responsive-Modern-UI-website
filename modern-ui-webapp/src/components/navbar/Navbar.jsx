1014d5560625f6bea8cf86c021cff78f6efa04e6
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 11:42:22 +0500

    Added css for logo image

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links-logo">
+        <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
       </div>
