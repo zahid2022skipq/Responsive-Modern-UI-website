cbfbc1932c94d6cfe5cf8aff253f972815eea97b
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 11:30:32 +0500

    Imported React icons

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./navbar.css";
-
+import { RiMenu3Line, RiCloseLine } from "react-icons";
 import logo from "../../assets/assets/logo.svg";
 
 const Navbar = () => {
