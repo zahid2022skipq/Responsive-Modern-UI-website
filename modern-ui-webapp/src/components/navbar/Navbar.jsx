49408f17387b9ee535946747f189044e957e6759
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-04 11:47:53 +0500

    Added anchor links for navbar menu

--- modern-ui-webapp/src/components/navbar/Navbar.jsx
+++ modern-ui-webapp/src/components/navbar/Navbar.jsx
@@ -15,16 +15,16 @@ const Navbar = () => {
             <a href="#home">Home</a>
           </p>
           <p>
-            <a href="#home">What is GPT?</a>
+            <a href="#wgpt3">What is GPT?</a>
           </p>
           <p>
-            <a href="#home">Open AI</a>
+            <a href="#possibility">Open AI</a>
           </p>
           <p>
-            <a href="#home">Case Studies</a>
+            <a href="#features">Case Studies</a>
           </p>
           <p>
-            <a href="#home">Library</a>
+            <a href="#blog">Library</a>
           </p>
         </div>
       </div>
