51ec00aa0cef52e92e24a91a546123163e57d466
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-08 17:11:49 +0500

    Designing WhatGPT Component

--- modern-ui-webapp/src/containers/whatGPT3/WhatGPT.jsx
+++ modern-ui-webapp/src/containers/whatGPT3/WhatGPT.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./whatgpt.css";
 
 const WhatGPT = () => {
-  return <div>WhatGPT</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading"></div>
+    </div>
+  );
 };
 
 export default WhatGPT;
