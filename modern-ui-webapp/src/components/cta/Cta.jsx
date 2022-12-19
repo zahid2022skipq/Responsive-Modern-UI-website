617d20e350504c6bd07a3e066135987856699677
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-19 23:19:05 +0500

    adding styles for CTA

--- modern-ui-webapp/src/components/cta/Cta.jsx
+++ modern-ui-webapp/src/components/cta/Cta.jsx
@@ -7,8 +7,8 @@ const Cta = () => {
         <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possiblities.</h3>
       </div>
-      <div className="gpt3__cta-button">
-        <button>Get Started</button>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
       </div>
     </div>
   );
