d2760579e01a32f692a8465ef431bc26c179eb6f
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-19 15:35:04 +0500

    Working with features component

--- modern-ui-webapp/src/containers/features/Features.jsx
+++ modern-ui-webapp/src/containers/features/Features.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./features.css";
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading"></div>
+    </div>
+  );
 };
 
 export default Features;
