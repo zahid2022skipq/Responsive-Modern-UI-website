8af18f610510f88550ed1552076b6261241843ef
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-19 16:06:59 +0500

    working on responsivenes of Features Container

--- modern-ui-webapp/src/containers/features/Features.jsx
+++ modern-ui-webapp/src/containers/features/Features.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./features.css";
 const Features = () => {
+  const featuresData = [
+    {
+      title: "Improving end distrusts instantly ",
+      text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+    },
+    {
+      title: "Become the tended active",
+      text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.",
+    },
+    {
+      title: "Message or am nothing",
+      text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.",
+    },
+    {
+      title: "Really boy law county",
+      text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.",
+    },
+  ];
   return (
     <div className="gpt3__features section__padding" id="features">
       <div className="gpt3__features-heading">
@@ -10,7 +29,11 @@ const Features = () => {
         </h1>
         <p>Request Early Access to Get Started</p>
       </div>
-      <div className="gpt3__features-container"></div>
+      <div className="gpt3__features-container">
+        {featuresData.map(({ title, text }) => (
+          <Feature key={title} title={title} text={text} />
+        ))}
+      </div>
     </div>
   );
 };
