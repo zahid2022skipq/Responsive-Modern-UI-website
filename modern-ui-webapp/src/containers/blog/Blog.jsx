40b18740310a8ebcd0291e091972f13c0aee110e
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-20 14:19:05 +0500

    working with blog section

--- modern-ui-webapp/src/containers/blog/Blog.jsx
+++ modern-ui-webapp/src/containers/blog/Blog.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./blog.css";
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container-groupA"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
