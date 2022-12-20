2e65160387cc8494772ecfb6692cf52933eb511f
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-20 14:35:50 +0500

    added dates and titles for blog article

--- modern-ui-webapp/src/components/article/Article.jsx
+++ modern-ui-webapp/src/components/article/Article.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./article.css";
-const Article = ({ imageUrl }) => {
+const Article = ({ imageUrl, title, date }) => {
   return (
     <div className="gpt3__blog-container_article">
       <div className="gpt3__blog-container_article-image">
         <img src={imageUrl} alt="blogs" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read full article</p>
       </div>
     </div>
   );
