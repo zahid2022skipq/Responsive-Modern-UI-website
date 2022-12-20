8606b8d09637e30074946db93b5c4c4d86dcaedf
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-20 14:25:02 +0500

    working with article container of image

--- modern-ui-webapp/src/components/article/Article.jsx
+++ modern-ui-webapp/src/components/article/Article.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./article.css";
-const Article = () => {
+const Article = ({ imageUrl }) => {
   return (
     <div className="gpt3__blog-container_article">
-      <div className="gpt3__blog-container_article-image"></div>
+      <div className="gpt3__blog-container_article-image">
+        <img src={imageUrl} alt="blogs" />
+      </div>
     </div>
   );
 };
