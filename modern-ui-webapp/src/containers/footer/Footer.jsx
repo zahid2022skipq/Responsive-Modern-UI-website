acb94a14f29973d2f7af8ca72f142ce60ae02a43
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-20 16:11:41 +0500

    working on footer

--- modern-ui-webapp/src/containers/footer/Footer.jsx
+++ modern-ui-webapp/src/containers/footer/Footer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import logo from "../../assets/assets/logo.svg";
+
 import "./footer.css";
 const Footer = () => {
   return (
@@ -7,6 +10,18 @@ const Footer = () => {
         <h1 className="gradient__text">
           Do you want to step in to the future before others
         </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+
+        <div className="gpt3__footer-links_div"></div>
       </div>
     </div>
   );
