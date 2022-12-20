a5c5374d9989a28f800b26b4170515c422c06b0c
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-20 14:23:51 +0500

    working with article container of image

--- modern-ui-webapp/src/components/article/Article.jsx
+++ modern-ui-webapp/src/components/article/Article.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./article.css";
 const Article = () => {
-  return <div>article</div>;
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image"></div>
+    </div>
+  );
 };
 
 export default Article;
